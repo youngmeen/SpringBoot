bb42f8c59b0e540e0488b15f0f851bf49dc66b85
김영민 <38073220+youngmeen@users.noreply.github.com>
2019-11-19 03:20:50 +0900

    re

--- boot02/src/main/java/org/zerock/persistence/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package org.zerock.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-import org.zerock.domain.Board;
-
-public interface BoardRepository extends CrudRepository<Board, Long>{
-
-}
